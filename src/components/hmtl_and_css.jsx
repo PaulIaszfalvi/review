0c6241439e6e2f057e6471c371d909de76f67728
paul <paul.iaszfalvi@gmail.com>
2023-05-01 15:07:43 -0400

    done

---
+++ src/components/hmtl_and_css.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function hmtl_and_css() {
+  return (
+    <div>
+        
+        <ul>
+            <li>Monday</li>
+            <li>Tuesday</li>
+            <li>Wednesday</li>
+        </ul>
+
+        <ol>
+            <li>Monday</li>
+            <li>Tuesday</li>
+            <li>Wednesday</li>
+        </ol>
+
+
+    </div>
+  )
+}
+
+export default hmtl_and_css
